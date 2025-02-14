96d0a9955ebb24f692755ea58dc3f82e9db695fe
bhuwanbhan <bhandaribhuwan880@gmail.com>
2025-02-14 10:44:03 +0545

    new

--- Re-Dash/src/App.jsx
+++ Re-Dash/src/App.jsx
@@ -1,17 +1,35 @@
 
 import './App.css'
 import Dash from './Components/DashBoard/Dash'
+import Login from './Components/Login/Login';
 import Navbar from './Components/Navbar/Navbar'
+import Reset from './Components/Reset/Reset';
 import Sidebar from './Components/Sidebar/Sidebar'
+import {  Routes, Route } from "react-router-dom";
 
 function App() {
   
 
   return (
-    <>
-  <Navbar/>
-  <Sidebar/>
-  <Dash/>
+     <>
+    {/* //    <Routes>
+    //      <Route path="/" element={<Dash />}> */}
+          {/* <Route path = "/navbar" element={<Navbar />} />
+          <Route path="/sidebar" element={<Sidebar />} /> */}
+{/*          
+          <Route path = "/Login" element={<Login />} />
+          <Route path="/Reset" element={<Reset />} /> */}
+
+        
+          
+      {/* //   </Route> */}
+      {/* //  </Routes>  */}
+
+      {/* <Login/> */}
+
+      <Reset/>
+    
+      
     </>
   )
 }
--- Re-Dash/src/Components/Navbar/Navbar.jsx
+++ Re-Dash/src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <div>
         
-        <div className = 'bg-gray-300 h-15  lg:h-20  border-1 '>
+        <div className = 'bg-gray-50 h-15  lg:h-16  border-y-1 '>
 
         </div>
 
